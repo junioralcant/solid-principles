b96a948e63efadb1f3b1995d5a5662c07dc0a627
junioralcant <junioralcant111@gmail.com>
2022-10-06 11:53:55 -0300

    Create order in memory

---
+++ src/database/order/order-inmemory.ts
@@ -0,0 +1,18 @@
+import { OrderGateway } from '../../gateways/order.gateway';
+import { CreateOrderGatewayDTO } from '../../presenters/order/create-order.gateway.dto';
+
+type OrderTypeInMemory = {
+  id: string;
+  document: string;
+  total: number;
+  createdAt: Date;
+  facilityId: string;
+};
+
+export class OrderInMemory implements OrderGateway {
+  orders: OrderTypeInMemory[] = [];
+
+  async save(data: CreateOrderGatewayDTO): Promise<void> {
+    this.orders.push(data);
+  }
+}
